1e1bd0a76f70ba5e9e0f44e79a11e7d38c0fc3a1
Scorpionno <jigoku27772@gmail.com>
2024-02-02 13:56:34 +0100

    zad 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,12 @@
     const list = document.querySelector('ul#categories');
     const elementsOfCategory = list.querySelectorAll('li.item');
+
+    console.log('Number of categories:', elementsOfCategory.length);
+   //console.log(elementsOfCategory);
+
+elementsOfCategory.forEach((category) => {
+    const allCategories = category.querySelector('h2').textContent; 
+    const allElements = category.querySelectorAll('li').length;
+    console.log(`Category: ${allCategories}`);
+    console.log(`Elements: ${allElements}`);
+      });
